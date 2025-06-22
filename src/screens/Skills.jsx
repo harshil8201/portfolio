65b5f0d9707aaa626bd0af787039b6562655aa95
harshil8201 <harshilpatel33h@gmail.com>
2025-06-22 15:38:37 -0400

    update

--- src/screens/Skills.jsx
+++ src/screens/Skills.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import SkillsHook from "../hooks/SkillsHook";
 import ReactSkill from "../assets/skills/react.png";
+import ReactNative from "../assets/skills/reactNative.png";
+import TypeScript from "../assets/skills/typescript.png";
+import Flutter from "../assets/skills/flutter.png";
+import AWS from "../assets/skills/AWS.png";
+import PostgreSQL from "../assets/skills/postgreSQL.png";
+import Redis from "../assets/skills/redis.png";
 import JavaScript from "../assets/skills/javascript.png";
 import HTML from "../assets/skills/html.png";
 import CSS from "../assets/skills/css.png";
 import TailwindCSS from "../assets/skills/tailwindcss.png";
 import NodeJs from "../assets/skills/nodejs.png";
-import MongoDB from "../assets/skills/mongodb.png";
+import MongoDB from "../assets/skills/mongoDB.png";
+import TensorFlow from "../assets/skills/tensorFlow.png";
 import Firebase from "../assets/skills/firebase.png";
 import MySql from "../assets/skills/mysql.png";
 import Figma from "../assets/skills/figma.png";
@@ -22,20 +29,27 @@ const Skills = () => {
         <p className="mb-7 uppercase text-3xl font-semibold text-colorRed">
           📌 my Skills
         </p>
-        <div className="flex flex-wrap gap-5 mt-5">
-          <SkillsHook skillName="React.js" skillUrl={ReactSkill} />
-          <SkillsHook skillName="Javascript" skillUrl={JavaScript} />
+        <div className="flex flex-wrap gap-8 mt-5 items-center justify-center">
+          <SkillsHook skillName="Java" skillUrl={Java} />
+          <SkillsHook skillName="Python" skillUrl={Python} />
           <SkillsHook skillName="HTML" skillUrl={HTML} />
           <SkillsHook skillName="CSS" skillUrl={CSS} />
+          <SkillsHook skillName="React.js" skillUrl={ReactSkill} />
+          <SkillsHook skillName="React Native" skillUrl={ReactNative} />
+          <SkillsHook skillName="Javascript" skillUrl={JavaScript} />
+          <SkillsHook skillName="Typescript" skillUrl={TypeScript} />
+          <SkillsHook skillName="Flutter" skillUrl={Flutter} />
           <SkillsHook skillName="Tailwind CSS" skillUrl={TailwindCSS} />
           <SkillsHook skillName="Node.js" skillUrl={NodeJs} />
           <SkillsHook skillName="MongoDB" skillUrl={MongoDB} />
+          <SkillsHook skillName="Redis" skillUrl={Redis} />
           <SkillsHook skillName="Firebase" skillUrl={Firebase} />
           <SkillsHook skillName="MySQL" skillUrl={MySql} />
+          <SkillsHook skillName="PostgreSQL" skillUrl={PostgreSQL} />
           <SkillsHook skillName="Figma" skillUrl={Figma} />
+          <SkillsHook skillName="AWS Service" skillUrl={AWS} />
+          <SkillsHook skillName="TensorFlow" skillUrl={TensorFlow} />
           <SkillsHook skillName="Wordpress" skillUrl={Wordpress} />
-          <SkillsHook skillName="Java" skillUrl={Java} />
-          <SkillsHook skillName="Python" skillUrl={Python} />
         </div>
       </div>
 
@@ -44,21 +58,27 @@ const Skills = () => {
         <p className="mb-7 uppercase text-xl font-semibold text-colorRed">
           📌 my Skills
         </p>
-        <div className="flex flex-wrap gap-5 mt-5">
-          <SkillsHook skillName="React.js" skillUrl={ReactSkill} />
-          <SkillsHook skillName="Javascript" skillUrl={JavaScript} />
+        <div className="flex flex-wrap gap-8 mt-5 items-center justify-center">
+          <SkillsHook skillName="Java" skillUrl={Java} />
+          <SkillsHook skillName="Python" skillUrl={Python} />
           <SkillsHook skillName="HTML" skillUrl={HTML} />
           <SkillsHook skillName="CSS" skillUrl={CSS} />
+          <SkillsHook skillName="React.js" skillUrl={ReactSkill} />
+          <SkillsHook skillName="React Native" skillUrl={ReactNative} />
+          <SkillsHook skillName="Javascript" skillUrl={JavaScript} />
+          <SkillsHook skillName="Typescript" skillUrl={TypeScript} />
+          <SkillsHook skillName="Flutter" skillUrl={Flutter} />
           <SkillsHook skillName="Tailwind CSS" skillUrl={TailwindCSS} />
           <SkillsHook skillName="Node.js" skillUrl={NodeJs} />
           <SkillsHook skillName="MongoDB" skillUrl={MongoDB} />
+          <SkillsHook skillName="Redis" skillUrl={Redis} />
           <SkillsHook skillName="Firebase" skillUrl={Firebase} />
           <SkillsHook skillName="MySQL" skillUrl={MySql} />
+          <SkillsHook skillName="PostgreSQL" skillUrl={PostgreSQL} />
           <SkillsHook skillName="Figma" skillUrl={Figma} />
+          <SkillsHook skillName="AWS Service" skillUrl={AWS} />
+          <SkillsHook skillName="TensorFlow" skillUrl={TensorFlow} />
           <SkillsHook skillName="Wordpress" skillUrl={Wordpress} />
-          <SkillsHook skillName="Java" skillUrl={Java} />
-          <SkillsHook skillName="Python" skillUrl={Python} />
-          
         </div>
       </div>
     </div>
